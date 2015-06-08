736c263774c594f9392b9e1ae6bd8cf2097f4043
PhazeIllusions <Guest@WTS5.local>
2015-06-07 17:28:08 -0700

    LV 0 ECLIPSE

---
+++ Lv 0/lvl 0/src/SimpleSquare.java
@@ -0,0 +1,19 @@
+import org.teachingextensions.logo.Tortoise;
+
+// Copyright Wintriss Technical Schools 2013
+public class SimpleSquare {
+	public void main(String[] args) {
+		// 1. Show the tortoise --#1
+		Tortoise.show();
+		Tortoise.hide();
+		Tortoise.move(29);
+		Tortoise.show();
+		Tortoise.setSpeed(6);
+		Tortoise.move(48);
+		// 6. Make the tortoise move as fast as possible --#6
+		// 5. Do the following 4 times --#5.1
+
+		// 2. Move the tortoise 50 pixels --#2
+		// 3. Turn the tortoise to the right (90 degrees) --#3
+	}
+}
