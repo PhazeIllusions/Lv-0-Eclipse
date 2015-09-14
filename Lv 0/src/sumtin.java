c5be4fd56e1ac3022894b6cdbb254d139b9a7090
PhazeIllusions <Guest@wts6.attlocal.net>
2015-09-13 17:28:41 -0700

    fawfawdfawfawfaw

---
+++ Lv 0/src/sumtin.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class sumtin {
+	public static void main(String[] args) {
+		String x = JOptionPane.showInputDialog("Please enter a random number");
+
+	}
+}
